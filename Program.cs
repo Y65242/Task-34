2aff6ff0381c29d1e3864a0cf1e04c7fe7a0b33a
Yerkin <144014337+Y65242@users.noreply.github.com>
2023-10-10 20:17:30 +0600

    Added task 34

--- Program.cs
+++ Program.cs
@@ -1,47 +1,38 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
-Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
+﻿/* Задача 34: Задайте массив заполненными случайными числами трехзначными числами. Напишите программу,
+которая определит количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
 
-int Exponentiation(int numberA, int numberB){
-  int result = 1;
-  for(int i=1; i <= numberB; i++){
-    result = result * numberA;
-  }
-    // int result = Math.Pow(numberA, numberB);
-    return result;
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
 }
 
-  Console.Write("Введите число A: ");
-  int numberA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numberB = Convert.ToInt32(Console.ReadLine());
 
-  int exponentiation = Exponentiation(numberA, numberB);
-  Console.WriteLine("Ответ: " + exponentiation);
+Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
 
-
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
-Console.Write("Введите число N: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-  int SumNumber(int numberN){
-    
-    int counter = Convert.ToString(numberN).Length;
-    int advance = 0;
-    int result = 0;
-
-    for (int i = 0; i < counter; i++){
-      advance = numberN - numberN % 10;
-      result = result + (numberN - advance);
-      numberN = numberN / 10;
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
     }
-   return result;
-  }
-
-int sumNumber = SumNumber(numberN);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
